9e646a9750eb219e3f1b01890294663e0413373a
SAN <sardarahmednazar@gmail.com>
2024-12-23 10:57:23 +0100

    done

--- SQL/python/lists_tuples.py
+++ SQL/python/lists_tuples.py
@@ -11,3 +11,29 @@
 #List items are ordered, changeable, and allow duplicate values.
 #List items are indexed, the first item has index [0], the second item has index [1] etc.
 #When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+
+
+fruit = ["apple", "banana", "cherry"]
+mixlist = ["apple", 1, 2, 3, "banana", 4, 5, "cherry", 6, 7, 8]
+print(fruit)
+print(mixlist)
+#Output: ['apple', 'banana', 'cherry']
+#Output: ['apple', 1, 2, 3, 'banana', 4, 5, 'cherry', 6, 7, 8]
+
+print(fruit[2])
+
+fruit[1] = "blackcurrant"
+print(fruit)
+
+# list methods
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value in the list
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the first item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list in ascending order
--- SQL/python/strings.py
+++ SQL/python/strings.py
@@ -17,29 +17,29 @@ print(name[2:])
 print(name[0:9:2])
 print(name[1:9:3])
 
-
-#sring functions
-
-a= "length"
-print(len(a))
-print(a.endswith("th"))
-print(a.startswith("le"))
-print(a.find("g"))
-print(a.count("e"))
-print(a.capitalize())
-print(a.upper())
-print(a.lower())
-print(a.replace("e","i"))
-print(a.title())
-print(a.swapcase())
-print(a.strip())
-print(a.lstrip())
-print(a.rstrip())
-print(a.split("e"))
-print(a.join("e"))
-print(a.isalnum())
-print(a.isalpha())
-print(a.isdigit())
+#
+##sring functions
+#
+#a= "length"
+#print(len(a))
+#print(a.endswith("th"))
+#print(a.startswith("le"))
+#print(a.find("g"))
+#print(a.count("e"))
+#print(a.capitalize())
+#print(a.upper())
+#print(a.lower())
+#print(a.replace("e","i"))
+#print(a.title())
+#print(a.swapcase())
+#print(a.strip())
+#print(a.lstrip())
+#print(a.rstrip())
+#print(a.split("e"))
+#print(a.join("e"))
+#print(a.isalnum())
+#print(a.isalpha())
+#print(a.isdigit())
 
 
 
